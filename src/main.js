333f2e88de8726400560d9e89de18c2039a2f2cb
MFAC <ninafroes@gmail.com>
2019-03-24 18:48:54 +0100

    "fix bugs"

--- src/main.js
+++ src/main.js
@@ -1,73 +1,37 @@
 const palindrome = () => {
 //Get the user input
+  const icon = document.getElementById("icon");
+  const message = document.getElementById("message");
+
   let input = document.getElementById("textarea").value;
-  console.log(input);
+  
   if(!input) {
-    document.getElementById("errorMessage").classList.add("show");
-    console.log("No input");
-    throw new Error(errorMessage);
+    icon.setAttribute('class', 'far fa-meh-rolling-eyes');
+    message.innerText = "Ops! There's nothing to check! Try to type something.";
+    return; 
   }
-//Array of alphanumerics characters to compare with the input array
-  const alphaNum = ('abcdefghijklmnopqrstuvwxyz0123456789').split('');
+  
+  const checkPalindrome = str => {
+    const alphaNumStr = str.toLowerCase().replace(/[\W_]/g, '');
 
-//Turn input into an array and into lowercase
-  const inputArray = input.toLowerCase().split('');
+    const reversedStr = alphaNumStr.split('').reverse().join('');
 
-//Remove all non-alphanumeric characters (punctuation, spaces and symbols)
-  const getAlphanum = (array1, array2) => {
-    let newArray = [];
-    for (let i = 0; i < array1.length; i++ ){
-      for (let j = 0; j < array2.length; j++){
-        if (array1[i] === array2[j]){
-          newArray.push(array1[i]);
-        };
-      };
-    };
-    return newArray;
-  };
+    const result = (reversedStr === alphaNumStr) ? true : false;
+    
+    return result; 
+  }
+  
+  
 
-  const finalArray = getAlphanum(inputArray, alphaNum);
-
-//Get reverse Array - copying the array first, because .reserve() is destructive
-  const getReverseArray = (arr) => {
-    let copyArr = [...arr]; //Spread operator to copy the original array
-    const newArray = copyArr.reverse();
-    return newArray;
-  };
-
-  const reverseArray = getReverseArray(finalArray);
-
-//Compare finalArray and reverseArray
-  const compareArrays = (array1, array2) => {
-    for (let i = 0; i < array1.length; i++ ){
-      if (array1[i] !== array2[i]){
-        return false;
-      };
-    };
-    return true;
-  };
-
-  let result = compareArrays(finalArray, reverseArray);
-
-  if(result) {
-    document.getElementById("isPalindrome").classList.add("show");
-    console.log("Palindrome");
+  if (checkPalindrome(input)) {
+    icon.setAttribute('class', 'far fa-grin-stars');
+    message.innerText = 'Yep! Definitely a palindrome!';
+    
   } else {
-    document.getElementById("notPalindrome").classList.add("show");
-    console.log("Not a Palindrome");
+    icon.setAttribute('class', 'far fa-sad-cry');
+    message.innerText = 'Nope! Not a palindrome! Try again.';
+    
   };
+  
 };
 
-/* If one of the results is showing, hide it */
-const hideResult = () => {
-  if (!event.target.closest("#textarea")) {
-    const results = document.getElementsByClassName("result");
-    let i;
-    for (i = 0; i < results.length; i++) {
-      let itemResult = results[i];
-      if (itemResult.classList.contains('show')) {
-        itemResult.classList.remove('show');
-      }
-    }
-  }
-};
